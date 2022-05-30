9b60160982e307acd9ab97e29ca729072a0d53e4
sandoellytonj <sandoellytonj@gmail.com>
2022-05-30 15:17:43 -0300

    adicionando o openAPI UI no gateway

--- cambio/src/main/java/com/cambio/cambioservice/CambioServiceApplication.java
+++ cambio/src/main/java/com/cambio/cambioservice/CambioServiceApplication.java
@@ -3,11 +3,16 @@ package com.cambio.cambioservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class CambioServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CambioServiceApplication.class, args);
+
 	}
 
 }
